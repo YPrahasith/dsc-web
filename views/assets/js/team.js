96113edbd230038c0ac930e3470b2b841caecda3
jayeshvjain <jayvjain1902@gmail.com>
2020-10-22 15:35:20 +0530

    Side Bar, Added colors

--- views/assets/js/team.js
+++ views/assets/js/team.js
@@ -3,3 +3,21 @@ $('.flip-card').on('click',
     $(this).toggleClass('flipped')
   }
 )
+var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768:{
+      direction: 'horizontal',
+    }
+  }
+});
